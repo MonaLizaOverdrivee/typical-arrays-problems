d672d6ccf70d58ffaa76f293c99b1d96b2d6a001
MonaLizaOverdrivee <santospele@mail.ru>
2021-07-09 16:01:28 +0300

    update argument name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
+exports.min = function min (num) {
   return num && num.length ? Math.min(...num) : 0
 }
 
-exports.max = function max (array) {
+exports.max = function max (num) {
   return num && num.length ? Math.max(...num) : 0
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg (num) {
   return num && num.length ? num.reduce((acc, itm) => acc + itm) /num.length : 0
 }
